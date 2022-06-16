d8e81a46cce97560d4243aea433c915583308796
Maria M <1sarenka1@gmail.com>
2022-06-16 22:26:42 +0200

    Initial Commit

---
+++ MedImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedImage
+//
+//  Created by Vanda S. on 16/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
